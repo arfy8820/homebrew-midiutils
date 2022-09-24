c2306515a3180400f5ee551fb459e25e88eec8ea
Arthur Pirika <arfy32@gmail.com>
2022-09-24 14:03:45 +0800

    Added various readmes and changelogs to the installed package, as well as the emacs mode.

--- Formula/pmml.rb
+++ Formula/pmml.rb
@@ -1,21 +1,31 @@
 class Pmml < Formula
-  desc "This is  the PMML(Practical Music Macro Language) compiler. The PMML compiler generates a MIDI file from MML(Music Macro Language) source code."
+  desc "This is  the PMML(Practical Music Macro Language) MIDI compiler"
   homepage "https://github.com/arfy8820/pmml"
   url "https://github.com/arfy8820/pmml/archive/refs/tags/v0.3.tar.gz"
   sha256 "72c6a80b153e04eeb1203a09cb425f7ea7d287854858f7f6664c656b980d57f0"
   license "GPL-2.0"
 
-    depends_on "texinfo" => :build
+  depends_on "texinfo" => :build
 
-	def install
+  def install
     system "make", "BINDIR=#{bin}", "LIBDIR=#{lib}"
-	# temporarily rename INSTALL to INSTALL.eng so that make install runs.
-	mv "INSTALL", "INSTALL.eng"
-	system "make", "BINDIR=#{bin}", "LIBDIR=#{lib}", "install"
-	mv "INSTALL.eng", "INSTALL"
-	cd "manual/eng"
-	system "#{HOMEBREW_PREFIX}/opt/texinfo/bin/texi2any", "-o", "pmml.info", "--no-split", "pmml.texi"
-	info.install "pmml.info"
+    # temporarily rename INSTALL to INSTALL.eng so that make install runs.
+    mv "INSTALL", "INSTALL.eng"
+    system "make", "BINDIR=#{bin}", "LIBDIR=#{lib}", "install"
+    mv "INSTALL.eng", "INSTALL"
+    prefix.install "INSTALL", "INSTALL.j", "README.j"
+    share.install "examples"
+    (share/"c_lib").mkpath
+    (share/"c_lib").install "c_lib/README"
+    (share/"comp").mkpath
+    (share/"comp").install "comp/ChangeLog", "comp/TODO"
+    (share/"comp").install "comp/tests"
+    elisp.install "emacs/pmml-mode.el"
+    (share/"m2p").mkpath
+    (share/"m2p").install "m2p/ChangeLog"
+    cd "manual/eng"
+    system HOMEBREW_PREFIX/"opt/texinfo/bin/texi2any", "-o", "pmml.info", "--no-split", "pmml.texi"
+    info.install "pmml.info"
   end
 
   test do
